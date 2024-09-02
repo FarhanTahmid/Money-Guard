45f8990274d381b437bd57b988b08f40d2e433da
Sakib2186 <skmdsakib2186@gmail.com>
2024-09-02 17:06:07 +0600

    .

---
+++ app_user/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+app_name = 'app_user'
+
+urlpatterns = [
+    path('login/', views.UserLogin.as_view(),name='login'),
+    path('signup/<str:email_signup>',views.UserRegistration,name='email_signup'),
+    path('api/user/', views.UserDetailView.as_view(), name='user-detail'),
+
+]
--- money_guard/urls.py
+++ money_guard/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include,re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
+    path('app_user/',include('app_user.urls',namespace='app_user')),
+
 ]
