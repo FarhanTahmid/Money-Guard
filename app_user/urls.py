b15b3b5d4361b9b824c2a9b7e960aa04bc69acac
Sakib2186 <skmdsakib2186@gmail.com>
2024-09-03 19:53:24 +0600

    done with email signup and google signup

--- app_user/urls.py
+++ app_user/urls.py
@@ -6,7 +6,7 @@ app_name = 'app_user'
 
 urlpatterns = [
     path('login/', views.UserLogin.as_view(),name='login'),
-    path('signup/<str:email_signup>',views.UserRegistration,name='email_signup'),
+    path('signup/email_signup/',views.UserRegistration,name='email_signup'),
     path('api/user/', views.UserDetailView.as_view(), name='user-detail'),
 
 ]
--- app_user/views.py
+++ app_user/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User,auth
 from django.contrib.auth import authenticate
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
 
-EMAIL_SIGNUP = 'email_signup'
-SOCIAL_SIGNUP = 'social_signup'
+
 # Create your views here.
 class UserLogin(APIView):
 
@@ -42,37 +41,35 @@ class UserLogin(APIView):
                 status=status.HTTP_401_UNAUTHORIZED)
         
 @api_view(['POST'])
-def UserRegistration(request,signup_method = None):
+@permission_classes([AllowAny])
+def UserRegistration(request):
+        
+    data = request.data
+    # get username password and email
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
 
-    if signup_method == EMAIL_SIGNUP:
-        
-        data = request.data
-        # get username password and email
-        username = data.get('username')
-        email = data.get('email')
-        password = data.get('password')
-
-        # Check if the username and email are already taken
-        if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
-            return Response({'Invalid': 'User Name Alreadt Exists. Try Loggin In'}, status=status.HTTP_401_UNAUTHORIZED)
-        
-        # Creating new u
-        try:
-            new_user = User.objects.create(username=username,email=email,password=password)
-            new_user.save()
-            return Response({'success': f'Signup Successful. Username, {username}, created'}, status=status.HTTP_201_CREATED)
-        
-        except Exception as e:
-            return Response({'Error': 'Something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
+    # Check if the username and email are already taken
+    if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+        return Response({'Invalid': 'User Name Alreadt Exists. Try Loggin In'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+    # Creating new u
+    try:
+        new_user = User.objects.create(username=username,email=email,password=password)
+        new_user.save()
+        return Response({'success': f'Signup Successful. Username, {username}, created'}, status=status.HTTP_201_CREATED)
+    
+    except Exception as e:
+        return Response({'Error': 'Something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
         
 class UserDetailView(APIView):
-    permission_classes = [IsAuthenticated]
 
-    def get(self, request):
-        user = request.user
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, *args, **kwargs):
         data = {
-            "id": user.id,
-            "username": user.username,
-            "email": user.email,
+            'user':request.user.username,
+            'email':request.user.email,
         }
-        return Response(data)
+        return Response(data, status=status.HTTP_200_OK)
